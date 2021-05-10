071bc060a0b75e84e8843fc723e1103f95d8d32a
Zayon Nieves <zayon_nieves@outlook.com>
2021-05-10 16:58:27 -0400

    Initial commit

---
+++ cheese-mvc/src/main/java/com/example/cheese/MyMVC/MyMVCApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheese.MyMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMVCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMVCApplication.class, args);
+	}
+
+}
---
+++ cheese-mvc/src/main/java/com/example/cheese/MyMVC/controllers/MyMVCController.java
@@ -0,0 +1,68 @@
+package com.example.cheese.MyMVC.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.ArrayList;
+
+
+@Controller
+@RequestMapping("MyMVC")
+public class MyMVCController {
+
+    ArrayList<String> groceries = new ArrayList<>();
+
+    // Request Path is set to: "/MyMVC" instead of default: "/"
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+        model.addAttribute("tabTitle", "Index");
+        model.addAttribute("pageTitle","Homepage");
+        // Returns index.html
+        return "MyMVC/index";
+    }
+
+    // List Page: Read CRUD function = GET HTTP method
+    @RequestMapping(value="list", method = RequestMethod.GET)
+    public String List(Model model){
+
+        model.addAttribute("groceries", groceries);
+        model.addAttribute("tabTitle","List");
+        model.addAttribute("pageTitle","Current List");
+        return "MyMVC/list";
+
+    }
+
+
+    // Read CRUD function = GET HTTP method
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model){
+        model.addAttribute("tabTitle","Add");
+        model.addAttribute("pageTitle", "Add Groceries");
+        return "MyMVC/add";
+
+    }
+
+
+    // Create CRUD function = POST HTTP method
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public RedirectView processAddGroceryForm(@RequestParam String groceryName){
+        groceries.add(groceryName);
+
+        // Redirect to "/myMVC/list" after action is committed
+        return new RedirectView("list");
+
+    }
+
+
+
+
+
+
+
+
+}
